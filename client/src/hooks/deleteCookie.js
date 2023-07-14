5af50bad90e623434bbb2824b8b323b156eae0a3
Akhil MAddineni <61157442+Akhil-Maddineni23@users.noreply.github.com>
2023-07-14 22:43:48 +0530

    Delete client directory

--- client/src/hooks/deleteCookie.js
+++
@@ -1,7 +0,0 @@
-export const deleteCookie = (noteID , removeCookie) => {
-    //const regex = new RegExp(`^shared-note-${userID}`);
-    //Delete the cookie
-    const cookieName = `shared-note-${note._id}`;
-    console.log("Cookie Name to be Deleted = ",cookieName);
-    removeCookie(cookieName);
-};
